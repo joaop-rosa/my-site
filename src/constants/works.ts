52bcb86787315f444b6c7b2bc2f3aba65d2a408f
joao.rosa <jp.ragdool@hotmail.com>
2026-05-22 00:16:40 -0300

    adjusted layout and added life

--- src/constants/works.ts
+++ src/constants/works.ts
@@ -2,9 +2,17 @@ export const WORKS = [
 	{
 		company: "CWI Software",
 		companyIcon: "/icons/cwi.svg",
+		role: "Desenvolvedor Front-end",
+		yearStart: 2024,
+		yearEnd: null,
+		iconBackground: "var(--black)",
+	},
+	{
+		company: "CWI Software",
+		companyIcon: "/icons/cwi.svg",
 		role: "Estágio Desenvolvedor FrontEnd",
 		yearStart: 2022,
-		yearEnd: null,
+		yearEnd: 2024,
 		iconBackground: "var(--black)",
 	},
 	{
